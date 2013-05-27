7d020a7c4e8f353bad1245c3a9bf4286f23d526e
Song Peng <songpeng@Songs-MacBook-Pro.local>
2013-05-27 15:04:03 +0800

    337 campaign separated by android/iOs

--- ElexTech/config.py
+++ ElexTech/config.py
@@ -2,13 +2,14 @@ SQL_CONFIG =\
 [\
 
     {'name':'ad spend',\
-     'sql':'''select par.offer_name, ctr.country_name, count(*), sum(convs.advertiser_amount*-0.01)
+     'sql':'''select par.offer_name, app.app_platform, ctr.country_name, count(*), sum(convs.advertiser_amount*-0.01)
               from analytics.conversions convs  
               join mstr.country_dimension ctr on ctr.country_iso3661_code=convs.country
               join analytics.offers_partners par on convs.advertiser_offer_id=par.offer_id
+              join analytics.apps_partners app on convs.publisher_app_id=app.app_id
               where par.partner_id='51bd38ac-b226-4ad7-a566-992b0ab862e3' and date(convs.created_at)=CURRENT_DATE-2
-              group by 1,2 
-              order by 1,2,3 DESC''', \
+              group by 1,2,3 
+              order by 1,2,3,4 DESC''', \
       'row':0, \
       'col':0,
     }, \
