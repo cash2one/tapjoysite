4eb2baecf7a92e026ac9c774deb10768120a8648
Song Peng <songpeng@Songs-MacBook-Pro.local>
2013-10-14 15:10:04 +0800

    add rae, wendy

--- DailyAdSpend/report.py
+++ DailyAdSpend/report.py
@@ -1,55 +1,61 @@
 #!/usr/bin/env python
 
-#-*-coding:utf8-*- 
-import xlwt, os, sys, datetime
+#-*-coding:utf8-*-
+import xlwt
+import os
+import sys
+import datetime
 import config
 from pyvertica.connection import get_connection
 
 
+import smtplib
+import mimetypes
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
 
-import  smtplib, mimetypes 
-from email.mime.text import MIMEText 
-from email.mime.image import MIMEImage 
-from email.mime.multipart import MIMEMultipart 
+#MAIL_LIST = ["song.peng@tapjoy.com", 'ming.wang@tapjoy.com']
+MAIL_FROM = 'Penguin' + "<" + 'song.peng' + "@" + 'tapjoy.com.cn' + ">"
 
-MAIL_LIST = ["song.peng@tapjoy.com", 'ming.wang@tapjoy.com'] 
-MAIL_FROM = 'Penguin' + "<"+ 'song.peng' + "@" + 'tapjoy.com.cn' + ">"
 
-def send_mail(subject, content, files = None): 
-    try: 
-        message = MIMEMultipart() 
-        message.attach(MIMEText(content)) 
-        message["Subject"] = subject 
-        message["From"] = MAIL_FROM 
-        message["To"] = ";".join(MAIL_LIST) 
-        for filename in files:
-            if filename != None and os.path.exists(filename): 
-                ctype, encoding = mimetypes.guess_type(filename) 
-                if ctype is None or encoding is not None: 
+def send_mail(subject, content, mailjobs=None):
+    try:
+        message = MIMEMultipart()
+        message.attach(MIMEText(content))
+        message["Subject"] = subject
+        message["From"] = MAIL_FROM
+        for (filename, maillist) in mailjobs.items():
+            if filename != None and os.path.exists(filename):
+                message["To"] = ";".join(maillist)
+                ctype, encoding = mimetypes.guess_type(filename)
+                if ctype is None or encoding is not None:
                     ctype = "application/octet-stream"
-                maintype, subtype = ctype.split("/", 1) 
-                attachment = MIMEImage((lambda f: (f.read(), f.close()))(open(filename, "rb"))[0], _subtype = subtype) 
-                attachment.add_header("Content-Disposition", "attachment", filename = os.path.split(filename)[-1]) 
-                message.attach(attachment) 
+                maintype, subtype = ctype.split("/", 1)
+                attachment = MIMEImage((lambda f: (f.read(), f.close()))(
+                    open(filename, "rb"))[0], _subtype=subtype)
+                attachment.add_header(
+                    "Content-Disposition", "attachment", filename=os.path.split(filename)[-1])
+                message.attach(attachment)
 
-        smtp = smtplib.SMTP() 
-        smtp.connect() 
-        smtp.sendmail(MAIL_FROM, MAIL_LIST, message.as_string()) 
-        smtp.quit() 
+        smtp = smtplib.SMTP()
+        smtp.connect()
+        smtp.sendmail(MAIL_FROM, MAIL_LIST, message.as_string())
+        smtp.quit()
 
         return True
-    except Exception, errmsg: 
-        print "Send mail failed to: %s" % errmsg 
+    except Exception, errmsg:
+        print "Send mail failed to: %s" % errmsg
         return False
 
 
-def AddSheet(wb, sheet, cr, orig_row = 0, orig_col = 0):
+def AddSheet(wb, sheet, cr, orig_row=0, orig_col=0):
     rows = cr.fetchall()
     if len(rows) < 1:  # no record
-        return False  
+        return False
     ws0 = wb.add_sheet(sheet)
 
-    row_num = orig_row 
+    row_num = orig_row
     column_num = orig_col
 
     for cell in rows[0].cursor_description:
@@ -58,7 +64,7 @@ def AddSheet(wb, sheet, cr, orig_row = 0, orig_col = 0):
 
     row_num = +1
     for row in rows:
-        column_num = orig_col 
+        column_num = orig_col
         for cell in row:
             ws0.write(row_num, column_num, cell)
             column_num += 1
@@ -66,37 +72,42 @@ def AddSheet(wb, sheet, cr, orig_row = 0, orig_col = 0):
 
     return True
 
+
 def main():
     settings = config.SQL_CONFIG
-    connection = get_connection(dsn = 'VerticaDSN', unicode_results='True')
+    connection = get_connection(dsn='VerticaDSN', unicode_results='True')
     cursor = connection.cursor()
 
-    files = [];
+    mailjobs = {}
     day = ''
     try:
-        cursor.execute('select year(CURRENT_DATE - 2), month(CURRENT_DATE - 2), day(CURRENT_DATE - 2 )')
+        cursor.execute(
+            'select year(CURRENT_DATE - 2), month(CURRENT_DATE - 2), day(CURRENT_DATE - 2 )')
         rows = cursor.fetchall()
-        month = '{0:4d}{1:02d}'.format(rows[0].year, rows[0].month);
-        day = '{0:4d}{1:02d}{2:02d}'.format(rows[0].year, rows[0].month, rows[0].day)
-        
+        month = '{0:4d}{1:02d}'.format(rows[0].year, rows[0].month)
+        day = '{0:4d}{1:02d}{2:02d}'.format(
+            rows[0].year, rows[0].month, rows[0].day)
+
         BASEDIR = os.path.join(os.path.realpath(sys.path[0]), 'report')
         for setting in settings:
             folder = os.path.join(BASEDIR, setting['partnername'], month)
-            filename = os.path.join(BASEDIR, setting['partnername'], month, setting['partnername'] + '_' + day) + '.xls'
-        
-    	    if not os.path.exists(folder):
-                os.makedirs(folder)	
-           
+            filename = os.path.join(
+                BASEDIR, setting['partnername'], month, setting['partnername'] + '_' + day) + '.xls'
+
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+
             wb = xlwt.Workbook()
-        
-            print str(datetime.datetime.now()) + '''  Pull data for "%s" ... ''' % setting['partnername'] 
-            #print setting['sql']
+
+            print str(datetime.datetime.now()) + '''  Pull data for "%s" ... ''' % setting['partnername']
+            # print setting['sql']
             cursor.execute(setting['sql'])
             if AddSheet(wb, setting['name'], cursor, setting['row'], setting['col']):
                 wb.save(filename)
-                files.append(filename)
+                mailjobs[filename] = setting['maillist']
+
             else:
-                print str(datetime.datetime.now()) + '''  No record for "%s" ''' % setting['partnername'] 
+                print str(datetime.datetime.now()) + '''  No record for "%s" ''' % setting['partnername']
 
     except Exception as ex:
         print str(datetime.datetime.now()) + '''  Pull data for "%s" failed''' % setting['partnername']
@@ -104,13 +115,11 @@ def main():
 
     finally:
         connection.close()
-     
+
     subject = day + ' report'
 
-    send_mail(subject, 'This is a daily report by Tapjoy.cn\n\n', files)
-
+    send_mail(subject, 'This is a daily report by Tapjoy.cn\n\n', mailjobs)
 
 
 if __name__ == "__main__":
     main()
-
