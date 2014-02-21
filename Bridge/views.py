2d92e9b13791f7129cc22768d9064ce307877bbf
Penguin Song <songpeng97@qq.com>
2014-02-21 15:28:19 +0800

    add app drive bridge

--- Bridge/views.py
+++ Bridge/views.py
@@ -21,17 +21,19 @@ def jump_snda(request):
 
 
 def jump_appdrive(request):
-    '''http://211.151.191.4:8765/channel/appdrive?campaign_id=712&idfa=123456781234456756781234567890ab'''
+    '''http://211.151.191.4:8765/channel/appdrive?campaign_id=712&idfa=123456781234456756781234567890ab&identifier=12345678-1234-4567-5678-1234567890ab'''
     campaign_id = request.REQUEST.get('campaign_id')
     deviceid = request.REQUEST.get('idfa')
+    identifier = request.REQUEST.get('identifier')
 
     idfa = '-'.join(
         [deviceid[:8], deviceid[8:12], deviceid[12:16], deviceid[16:20], deviceid[20:32]]).upper()
     site_id = '1960'
     source = '767'
+
     sitekey = 'a37c8373f4c87cf782b7618a00c07768'
 
-    redirect_url = 'http://appdriver.cn/6.0.%sac?source=%s&campaign_id=%s&returnFormat=3&idfa=%s' % (
-        site_id, source, campaign_id, idfa)
+    redirect_url = 'http://appdriver.cn/6.0.%sac?source=%s&campaign_id=%s&returnFormat=3&idfa=%s&identifier' % (
+        site_id, source, campaign_id, idfa, identifier)
 
     return HttpResponseRedirect(redirect_url)
