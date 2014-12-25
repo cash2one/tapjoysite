2d70287a33858b8c0488f05f26a38655942404a8
root <root@host1.tapjoy.com.cn>
2014-12-25 11:18:13 +0800

    last submit

--- Bridge/urls.py
+++ Bridge/urls.py
@@ -6,9 +6,4 @@ urlpatterns = patterns('',
                        url(r'^snda$', views.jump_snda, name='jumpsnda'),
                        url(r'^adwo$', views.jump_adwo, name='jumpadwo'),
                        url(r'^dianxin$', views.jump_dianxin, name='jumpdianxin'),
-                       url(r'^appdrive$', views.jump_appdrive,
-                           name='jumpappdrive'),
-                       url(r'^appdrive_postback$', views.postback_appdrive,
-                           name='postbackappdrive'),
-
                        )
